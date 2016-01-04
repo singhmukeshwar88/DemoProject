46df315a668945039415db99589f7e3b1e8bafa6
Amit Doshi <amit.doshi@synechorn.com>
2016-01-04 19:06:34 +0530

    Initial Changes

---
+++ src/org/mukesh/demo/DemoClass.java
@@ -0,0 +1,10 @@
+package org.mukesh.demo;
+
+public class DemoClass {
+
+	
+	public String demo(){
+	
+		return "Mukeshwar Singh";
+	}
+}
---
+++ src/org/mukesh/demo/MainClass.java
@@ -0,0 +1,13 @@
+package org.mukesh.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		DemoClass obj = new DemoClass();
+		System.out.println("Welcome User:"+obj.demo());
+		System.out.println("Changed files");
+
+	}
+
+}
